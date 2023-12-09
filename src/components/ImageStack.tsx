78568111c9c2b8f3e458f73a5ebf73da71f89b99
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-12-09 11:13:23 +0300

    Update ImageStack.tsx

--- src/components/ImageStack.tsx
+++ src/components/ImageStack.tsx
@@ -1,25 +1,19 @@
-// Import the images as modules using the named import syntax
-import { default as samuel } from "../assets/images/samuel.jpg";
-import { default as vazh } from "../assets/images/vazhnik.jpg";
-import { default as zemu } from "../assets/images/zemu.jpg";
-import { default as gitz } from "../assets/images/gitz.jpg";
-// An array of image modules
+import samuel from "../assets/images/samuel.jpg";
+import vazh from "../assets/images/vazhnik.jpg";
+import zemu from "../assets/images/zemu.jpg";
+import gitz from "../assets/images/gitz.jpg";
 const images = [samuel, zemu, vazh, gitz];
 
-// A React component that renders the images
 const ImageStack = () => {
   return (
-    // A container element with relative position
     <div className="relative">
       {images.map((image, index) => (
-        // An image element with Tailwind classes and the image module as the source
-        // Use the default property to access the image data
         <img
-          src={image.default}
+          src={image}
           alt=""
-          className={`rounded-full w-20 h-20 absolute top-${index * 4} left-${
+          className={`absolute inline-block rounded-full w-20 h-20 top-[${index * 4}] left-[${
             index * 4
-          } z-${index * 10}`}
+          }] z-[${index * 10}]`}
           key={index}
         />
       ))}
